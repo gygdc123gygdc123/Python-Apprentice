60a0321462e84e95f8ce69576943ac92a2e0d9db
gygdc123gygdc123 <jackleesd2023@gmail.com>
2026-01-30 21:32:12 +0000

    i did nothing this is a test

--- lessons/10_Turtles/20_Introducing_Tina/30_Squares_and_Circles/Squares_and_Circles.py
+++ lessons/10_Turtles/20_Introducing_Tina/30_Squares_and_Circles/Squares_and_Circles.py
@@ -12,7 +12,7 @@ turtle.setup(600,600,0,0)               # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast.
+tina.speed(7)                           # Make the turtle move as fast, but not too fast.
 
 ##
 ## Move Tina to the Starting Position
@@ -26,7 +26,7 @@ tina.pendown()                          # Put the pen down so we can draw
 ## Draw a Square
 ##
 
-tina.pencolor('blue')                   # Set the pen color to blue
+tina.pencolor('yellow')                   # Set the pen color to blue
 tina.forward(200)                       # Move tina forward by the forward distance
 tina.right(90)                          # Turn tina left by the left turn
 
