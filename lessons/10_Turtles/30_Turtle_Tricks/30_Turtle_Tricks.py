89b5db112f576f552729008b91603f401d834a55
gygdc123gygdc123 <jackleesd2023@gmail.com>
2026-01-31 01:31:50 +0000

    checking up on code jan 30, 5:31pm

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -13,7 +13,7 @@ to see how to use the turtle commands.
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600,600,0,0)               # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
-tina.speed(0)
+tina.speed(5)
 tina.begin_fill()
 tina.pendown()
 tina.circle(80)
@@ -32,7 +32,13 @@ tina.pendown()
 tina.begin_fill()
 tina.circle(50)
 tina.end_fill()
-
+tina.penup()
+tina.goto(0,)180
+tina.pendown()
+tina.begin_fill()
+tina.circle(20)
+tina.fillcolor("black")
+tina.end_fill()
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
 
